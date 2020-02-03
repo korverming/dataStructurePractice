cc098640beb591da2082b1833767358083f1e039
wuxm04 <wuxm04@126.com>
2020-02-03 21:43:34 +0800

    staticArray

---
+++ src/test/test19.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "StaticArray.h"
+
+using namespace std;
+using namespace DTLib;
+
+int main()
+{
+	StaticArray<int, 5> s1;
+
+	for (int i = 0; i < s1.length(); i++)
+	{
+		s1[i] = i * i;
+	}
+
+	for (int i = 0; i < s1.length(); i++)
+	{
+		cout << s1[i] << endl;
+	}
+
+	StaticArray<int, 5> s2;
+
+	s2 = s1;
+
+	for (int i = 0; i < s2.length(); i++)
+	{
+		cout << s2[i] << endl;
+	}
+
+	s2[6] = 100;
+
+	return 0;
+}
