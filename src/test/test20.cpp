d5e7b3b8d90e431df2fe935df659f9d7311a4def
wuxm04 <wuxm04@126.com>
2020-02-06 21:58:57 +0800

    optimize DynamicArray

--- src/test/test20.cpp
+++ src/test/test20.cpp
@@ -6,5 +6,30 @@ using namespace DTLib;
 
 int main()
 {
+	DynamicArray<int> s1(5);
+	for (int i = 0; i < s1.length(); i++)
+	{
+		s1[i] = i * i;
+	}
+
+	for (int i = 0; i < s1.length(); i++)
+	{
+		cout << s1[i] << endl;
+	}
+
+	DynamicArray<int> s2(10);
+
+	s2 = s1;
+
+	//s2.resize(8);
+	s2.resize(3);
+
+	for (int i = 0; i < s2.length(); i++)
+	{
+		cout << s2[i] << endl;
+	}
+
+	//s2[4] = 100;
+
 	return 0;
 }
